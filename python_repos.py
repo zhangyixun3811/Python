3d50adf90a99701be22cb7913833b20e90724472
zhangyixun3811 <f17800823697@163.com>
2021-02-09 02:57:31 +0000

    format the code

--- python_repos.py
+++ python_repos.py
@@ -1,24 +1,24 @@
 import requests
 
-#执行API调用并存储响应
+# 执行API调用并存储响应
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 print("Status code", r.status_code)
 
-#将API相应存储到变量
+# 将API相应存储到变量
 response_dict = r.json()
 print("Total repositories:", response_dict['total_count'])
 
-#探索有关仓库的信息
+# 探索有关仓库的信息
 repo_dicts = response_dict['items']
 print("repositories returned:", len(repo_dicts))
 
-#研究第一个仓库
+# 研究第一个仓库
 repo_dict = repo_dicts[0]
 
 print("\ninformation for first repository:")
-print("name:",repo_dict['name'])
-print("owner:",repo_dict['owner']['login'])
-print("Stars:",repo_dict['stargazers_count'])
-print("url:",repo_dict['html_url'])
-print("Descrip:",repo_dict['description'])
+print("name:", repo_dict['name'])
+print("owner:", repo_dict['owner']['login'])
+print("Stars:", repo_dict['stargazers_count'])
+print("url:", repo_dict['html_url'])
+print("Descrip:", repo_dict['description'])
--- timer.py
+++ timer.py
@@ -1,15 +1,18 @@
 import time as t
 
+
 class MyTimer:
-    #开始
+    # 开始
     def start(self):
         self.begin = t.localtime()
         print("计时开始！...")
-    #停止
+
+    # 停止
     def stop(self):
         self.end = t.localtime()
         print("计时结束！...")
-    #计算
+
+    # 计算
     def _calc(self):
         self.lasted = []
         self.prompt = '总共运行了'
@@ -17,4 +20,3 @@ class MyTimer:
             self.lasted.append(self.stop[index] - self.start[index])
             self.prompt += str(self.lasted[index])
         print(self.prompt)
-        
