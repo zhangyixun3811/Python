9049df202322d376c087468d5b3d733cf0270626
zhangyixun3811 <67584167+zhangyixun3811@users.noreply.github.com>
2021-02-11 13:52:43 +0800

    fix #1 Shoot

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -7,3 +7,8 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
+        # 子弹设置
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
